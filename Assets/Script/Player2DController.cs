5aaeb823847ac61d1477ab0fb9edfb6f456c4fbb
Darkvdz <famparintorn@gmail.com>
2026-04-23 10:33:35 +0700

    FullScriptPlayer

--- Assets/Script/Player2DController.cs
+++ Assets/Script/Player2DController.cs
@@ -5,7 +5,8 @@ public class Player2DController : MonoBehaviour
 {
     public float speed = 5.0f;
     public float jumpforce = 450;
-
+    private bool isGrounded;
+    private SpriteRenderer spriteRenderer;
 
     private Rigidbody2D rb;
     private float moveInputValue; // A = -1 / D = 1
@@ -13,6 +14,7 @@ public class Player2DController : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        spriteRenderer = rb.GetComponent<SpriteRenderer>();
     }
 
     
@@ -25,9 +27,43 @@ public class Player2DController : MonoBehaviour
         }
         rb.linearVelocity = new Vector2(moveInputValue * speed, rb.linearVelocity.y);
 
-        if (Keyboard.current.spaceKey.wasPressedThisFrame)
+        if (moveInputValue < 0)
+        {
+            spriteRenderer.flipX = true;
+        }
+        if (moveInputValue > 0)
+        {
+            spriteRenderer.flipX = false;
+        }
+
+
+        if (Keyboard.current.spaceKey.wasPressedThisFrame && isGrounded)
         {
             rb.AddForce(new Vector2(rb.linearVelocity.x, jumpforce));
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
         }
     }
 }
